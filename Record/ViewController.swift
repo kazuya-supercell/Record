6099efadb66601e37eed95a47e0126c9900b8a11
kazuya-supercell <kazuya150515@gmail.com>
2020-12-20 16:35:50 +0900

    Initial Commit

---
+++ Record/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Record
+//
+//  Created by 松浦和哉 on 2020/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
